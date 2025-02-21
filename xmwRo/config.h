146a15c3893aaec89939aa366b35f7449700f04e
ChiefPickleFace <tasinmamun@gmail.com>
2025-02-20 20:32:44 -0800

    Update config.h

--- xmwRo/config.h
+++ xmwRo/config.h
@@ -3,13 +3,11 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 
-#define PERMISSIVE_HOLD
-
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #define CAPS_LOCK_STATUS
-#define SERIAL_NUMBER "xmwRo/Qzzean"
+#define SERIAL_NUMBER "xmwRo/KrrWLz"
 #define LAYER_STATE_8BIT
-#define COMBO_COUNT 8
+#define COMBO_COUNT 2
 
 #define TAPPING_TERM_PER_KEY
 #define RGB_MATRIX_STARTUP_SPD 60
